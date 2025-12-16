991bc95081c13cdb3d36196bb5d1a4280c0b7518
Dav1nGen <davicheng1114@gmail.com>
2025-12-16 14:08:37 +0800

    fiexed

--- main.py
+++ main.py
@@ -8,25 +8,10 @@ import ctypes
 import threading
 import time
 import math
-from datetime import datetime
+from datetime import datetime 
+from lerobot.robots.jakaS12.modbus_tcp import ModbusTCP
 
-robot = jkrc.RC("192.168.1.5")
-robot.login()
-robot.power_on()
-robot.enable_robot()
-robot.edg_init_extend(en=True,
-                      edg_stat_ip="192.168.1.5",
-                      edg_port=10010,
-                      edg_mode=0)
-robot.servo_move_enable(1)
-        
-while(1):
-    pos=[0,0,0,0,0,0]
-    print(pos)
-    robot.edg_servo_p(end_pos=pos,
-                    move_mode=1,
-                    step_num=1,
-                    robot_index=0)
-    time.sleep(0.008)
+modbus = ModbusTCP()
 
-
+modbus.connect()
+modbus.write(8,1)
